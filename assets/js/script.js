14bd3e949dd4c349d15fd6825a935540a12956e3
turbopanchos <turbopanchos7@gmail.com>
2026-07-29 21:17:44 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
-// JavaScript de Turbo Panchos
+// Cambia el fondo de la navbar al hacer scroll
+window.addEventListener("scroll", () => {
+    const navbar = document.querySelector(".navbar");
+
+    if (window.scrollY > 50) {
+        navbar.style.background = "#121212";
+    } else {
+        navbar.style.background = "rgba(0,0,0,.55)";
+    }
+});
